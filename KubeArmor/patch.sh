b4e15003631f34590f10522d85bce54cbc7912ad
Barun Acharya <barun.acharya@accuknox.com>
2021-12-20 19:16:29 +0530

    Update patch.sh

--- KubeArmor/patch.sh
+++ KubeArmor/patch.sh
@@ -19,3 +19,5 @@ do
 		sed -i 's/C.bpf_attach_uprobe(C.int(fd), attachType, evNameCS, binaryPathCS, (C.uint64_t)(addr), (C.pid_t)(pid))/C.bpf_attach_uprobe(C.int(fd), attachType, evNameCS, binaryPathCS, (C.uint64_t)(addr), (C.pid_t)(pid), 0)/g' $GOPATH/pkg/mod/github.com/iovisor/$GOBPF/bcc/module.go
 	fi
 done
+
+
